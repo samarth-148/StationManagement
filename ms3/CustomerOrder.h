b0030dcb70f15014ff4975325f3b30d7e167c5de
samarth-148 <111608340+samarth-148@users.noreply.github.com>
2023-01-17 17:28:44 -0500

    Update CustomerOrder.h

--- ms3/CustomerOrder.h
+++ ms3/CustomerOrder.h
@@ -1,10 +1,6 @@
 // Name: Samarth Shaileshkumar Patel
-// Seneca Student ID: 143147213
 // Seneca email: sspatel74@seneca.ca
 // Date of completion: 11/16/2022
-//
-// I confirm that I am the only author of this file
-//   and the content was created entirely by me.
 
 #ifndef SDDS_CUSTOMERORDER_H
 #define SDDS_CUSTOMERORDER_H
